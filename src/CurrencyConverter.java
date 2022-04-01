2329049ef6f0ee0da296e570cd91be1149e5ef57
takat <takat@10.0.0.166>
2022-03-31 19:32:22 -0700

    "Adding Scanner

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -1,9 +1,43 @@
+import java.util.HashMap;
+import java.util.Scanner;
 
+/*
+ * Thien Trinh
+ * 
+ * This is a simple currency converter program.
+ */
 public class CurrencyConverter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		HashMap<Integer, String> currencyCodes = new HashMap<Integer, String>();
+		
+		// Add currency codes.
+		currencyCodes.put(1, "USD");
+		currencyCodes.put(2, "CAD");
+		currencyCodes.put(3, "EUR");
+		currencyCodes.put(4, "HKD");
+		currencyCodes.put(5, "INR");
 
+		String fromCode, toCode;
+		double amount;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Welcome to the currency converter!");
+		
+		System.out.println("Currency converting FROM?");
+		System.out.println("1:USD (US Dollar)\t 2:CAD (Canadian Dollar)\t 3:EUR (Euro)\t 4:HKD (Hong Kong Dollar)\t 5:INR (Indian Rupee)");
+		fromCode = currencyCodes.get(sc.nextInt());
+		
+		System.out.println("Currency converting TO?");
+		System.out.println("1:USD (US Dollar)\t 2:CAD (Canadian Dollar)\t 3:EUR (Euro)\t 4:HKD (Hong Kong Dollar)\t 5:INR (Indian Rupee)");
+		toCode = currencyCodes.get(sc.nextInt());
+		
+		System.out.println("Amount you with to convert?");
+		amount = sc.nextFloat();
+		
+		
+		System.out.println("Thank you for using the currency converter!");
 	}
-	
+
 }
